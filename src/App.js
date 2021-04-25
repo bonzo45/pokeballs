255acb3ccad791edd829944f3e883a51c409e40c
Sam Esgate <sam.esgate@sparx.co.uk>
2021-04-25 18:26:58 +0100

    Canvas seems more performant... nice...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import {Pokeball} from "./Pokeball";
+// import {Pokeball} from "./Pokeball";
+import {Pokeballs} from "./Pokeballs";
 
 function App() {
-    const pokeballs = [];
-    for (let i = 0; i < 898; i++) {
-        pokeballs.push(<Pokeball key={i} index={i} />);
-    }
+    // const pokeballs = [];
+    // for (let i = 0; i < 898; i++) {
+    //     pokeballs.push(<Pokeball key={i} index={i} />);
+    // }
     return (
         <div className="App">
             <header className="App-header">
-                {pokeballs}
+                {/*{pokeballs}*/}
+                {<Pokeballs />}
             </header>
         </div>
       );
---
+++ src/Pokeballs.js
@@ -0,0 +1,72 @@
+import { React, useEffect } from 'react';
+import anime from 'animejs';
+
+export const Pokeballs = () => {
+    const runStuff = () => {
+        var canvas = document.getElementById("poke-canvas");
+        var ctx = canvas.getContext("2d");
+
+        var img = document.getElementById("poke-img");
+        // const numbles = 251;
+        const numbles = 898;
+        console.log(anime.stagger(10))
+        let pokeballSize;
+        var state = {
+            currentPokeballSize: 0,
+        };
+        var pokeballStates = [];
+        for (let i = 0; i < numbles; i++) {
+            pokeballStates.push({
+                currentPokeballSize: 0,
+            })
+        }
+        const rows = 23;
+        const columns = 39;
+
+        function setCanvasSize() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            canvas.style.width = window.innerWidth + 'px';
+            canvas.style.height = window.innerHeight + 'px';
+            pokeballSize = window.innerWidth / columns;
+        }
+
+        const renderPokeballs = () => {
+            for (let i = 0; i < numbles; i++) {
+                const row = Math.floor(i / columns);
+                const column = i % columns;
+                const currentSize = pokeballStates[i].currentPokeballSize;
+                const left = (column * pokeballSize) + (pokeballSize / 2) - (currentSize / 2);
+                const top = (row * pokeballSize) + (pokeballSize / 2) - (currentSize / 2);
+                ctx.drawImage(img, left, top, currentSize, currentSize);
+            }
+        }
+
+        const animatePokeball = () => {
+            anime.timeline().add({
+                targets: pokeballStates,
+                currentPokeballSize: pokeballSize,
+                delay: function() { return anime.random(0, 2000); },
+                update: renderPokeballs,
+                duration: 1000,
+            });
+        }
+
+        const render = anime({
+            // duration: Infinity,
+            update: function() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+            },
+        })
+
+        setCanvasSize();
+        render.play();
+        animatePokeball();
+    }
+    return (
+        <>
+            <canvas id="poke-canvas"/>
+            <img style={{display: 'none'}} onLoad={runStuff} id="poke-img" src="pokeball.svg" />
+        </>
+    );
+}
