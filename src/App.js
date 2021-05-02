658a926598307430e017d5cdc181fb0633357933
Sam Esgate <sam.esgate@sparx.co.uk>
2021-05-02 21:57:17 +0100

    Add pokemon image and Reactify code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import {Pokeballs} from "./Pokeballs";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import pokemonCSV from '!!csv-loader!./pokemon.csv';
+
+import { Pokeballs } from "./Pokeballs";
+
+const pokemonList = [];
+let i = 1;
+for (const row of pokemonCSV) {
+    if (i !== 1) {
+        const name = row[3] || "";
+        pokemonList.push({
+            name: name,
+            imageName: `/pokemon/${name.toLowerCase().replace(/[^a-z]/gi, '')}.png`,
+        })
+    }
+    i++;
+}
 
 function App() {
     return (
         <div className="App">
-            <Pokeballs />
+            <Pokeballs pokemonList={pokemonList}/>
         </div>
       );
 }
--- src/Pokeballs.js
+++ src/Pokeballs.js
@@ -1,60 +1,72 @@
-import { React } from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 import anime from 'animejs';
 
-export const Pokeballs = () => {
-    let loaded = 0;
-    const numbles = 898;
-    const pokeballRotation = 25;
-    const numInGenerations = [151, 100, 135, 107, 156, 72, 88, 89];
-    const numInGenerationsTotal = [];
-    let total = 0;
-    for (let numInGeneration of numInGenerations) {
-        total += numInGeneration;
-        numInGenerationsTotal.push(total);
-    }
-    let highlightThreshold = numInGenerationsTotal[0];
-    const pokeballAppearTime = 1000;
-    const pokeballAppearDelay = 5000;
-    const pokeballWobbleTime = 1000;
-    const pokeballWobbleDelay = 400000;
+const NUMBER_OF_POKEMON = 898;
+const NUMBER_OF_POKEMON_PER_GENERATION = [151, 100, 135, 107, 156, 72, 88, 89];
+const NUMBER_OF_POKEMON_PER_GENERATION_TOTAL = [];
+let total = 0;
+for (let numInGeneration of NUMBER_OF_POKEMON_PER_GENERATION) {
+    total += numInGeneration;
+    NUMBER_OF_POKEMON_PER_GENERATION_TOTAL.push(total);
+}
 
-    let canvas;
-    let ctx;
-    const pokeballStates = [];
-    let pokeballImg;
-    let pokeballGreyscaleImg;
-    let pokeballSize;
-    let rows = 0;
-    let columns = 0;
-    function setCanvasSize() {
-        const canvasWidth = document.getElementById('poke-canvas-wrapper').clientWidth;
-        const canvasHeight = document.getElementById('poke-canvas-wrapper').clientHeight;
+const POKEBALL_ROTATION = 25;
+const POKEBALL_APPEAR_TIME = 1000;
+const POKEBALL_APPEAR_DELAY = 5000;
+const POKEBALL_WOBBLE_TIME = 1000;
+const POKEBALL_WOBBLE_DELAY = 400000;
+
+const pokeballStates = [];
+let highlightThreshold = NUMBER_OF_POKEMON_PER_GENERATION_TOTAL[0];
+let forceRender = false;
+
+export const Pokeballs = ({pokemonList}) => {
+    const [imagesLoaded, setImagesLoaded] = useState(0);
+    const [animating, setAnimating] = useState(false);
+    const [selectedPokemon, setSelectedPokemon] = useState(1);
+    const [, setRows] = useState(0);
+    const [columns, setColumns] = useState(0);
+    const [pokeballSize, setPokeballSize] = useState(0);
+
+    const canvasWrapperRef = useRef();
+    const canvasRef = useRef();
+    const ctxRef = useRef();
+    const pokeballImgRef = useRef();
+    const pokeballGreyscaleImgRef = useRef();
+
+    // Measure the screen size to work out the number of rows, columns and size of the pokeballs.
+    useEffect(() => {
+        const canvasWidth = canvasWrapperRef.current.clientWidth;
+        const canvasHeight = canvasWrapperRef.current.clientHeight;
         const aspectRatio = canvasWidth / canvasHeight;
-        columns = Math.sqrt(numbles * aspectRatio);
-        rows = columns / aspectRatio;
-        columns = Math.ceil(columns);
-        rows = Math.ceil(rows);
-        pokeballSize = Math.min(canvasWidth / columns, canvasHeight / rows);
+        let _columns = Math.sqrt(NUMBER_OF_POKEMON * aspectRatio);
+        let _rows = _columns / aspectRatio;
+        _columns = Math.ceil(_columns);
+        _rows = Math.ceil(_rows);
+        setColumns(_columns);
+        setRows(_rows);
+        setPokeballSize(Math.min(canvasWidth / _columns, canvasHeight / _rows));
 
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-        canvas.style.width = canvasWidth + 'px';
-        canvas.style.height = canvasHeight + 'px';
-    }
+        canvasRef.current.width = canvasWidth;
+        canvasRef.current.height = canvasHeight;
+        canvasRef.current.style.width = canvasWidth + 'px';
+        canvasRef.current.style.height = canvasHeight + 'px';
+    }, [canvasWrapperRef, canvasRef])
 
-    const createRenderPokeballs = (alwaysRender = false) => () => {
-        for (let i = 0; i < numbles; i++) {
+    const renderPokeballs = useCallback(() => {
+        for (let i = 0; i < NUMBER_OF_POKEMON; i++) {
             const row = Math.floor(i / columns);
             const column = i % columns;
 
             // Only bother re-rendering the ball if the size or rotation isn't in the end state.
             const pokeballState = pokeballStates[i];
-            if (pokeballState.currentPokeballSize === pokeballSize && pokeballState.rotation === 0 && !alwaysRender) {
+            // alert(pokeballSize);
+            if (pokeballState.currentPokeballSize === pokeballSize && pokeballState.rotation === 0 && !forceRender) {
                 continue;
             }
 
             // Clear the part of the canvas that corresponds to this pokeball (and the space it can roll into).
-            ctx.clearRect(
+            ctxRef.current.clearRect(
                 column * pokeballSize,
                 row * pokeballSize,
                 pokeballSize + 2,
@@ -63,102 +75,111 @@ export const Pokeballs = () => {
 
             const scaledSize = pokeballState.currentPokeballSize * 0.80;
 
-            ctx.save();
+            ctxRef.current.save();
             // Crazy rotation...
             const adjustmentForRotation = (Math.PI * scaledSize * (pokeballState.rotation / 360));
             const halfPokeballSize = pokeballSize / 2;
             const getToCenterLeft = (column * pokeballSize) + halfPokeballSize + adjustmentForRotation;
             const getToCenterTop = (row * pokeballSize) + halfPokeballSize;
-            ctx.translate(getToCenterLeft, getToCenterTop);
-            ctx.rotate(pokeballState.rotation * Math.PI / 180)
-            ctx.translate(-getToCenterLeft, -getToCenterTop);
+            ctxRef.current.translate(getToCenterLeft, getToCenterTop);
+            ctxRef.current.rotate(pokeballState.rotation * Math.PI / 180)
+            ctxRef.current.translate(-getToCenterLeft, -getToCenterTop);
 
             // Then draw it as normal...
             const halfCurrentSize = scaledSize / 2
             const left = getToCenterLeft - halfCurrentSize;
             const top = getToCenterTop - halfCurrentSize;
-            const image = i < highlightThreshold ? pokeballImg : pokeballGreyscaleImg;
-            ctx.drawImage(image, left, top, scaledSize, scaledSize);
-            ctx.restore();
+            const image = i < highlightThreshold ? pokeballImgRef.current : pokeballGreyscaleImgRef.current;
+            ctxRef.current.drawImage(image, left, top, scaledSize, scaledSize);
+            ctxRef.current.restore();
         }
+
+        forceRender = false;
+    }, [columns, highlightThreshold, pokeballSize]);
+
+    const imageLoaded = () => {
+        setImagesLoaded(imagesLoaded + 1);
     }
 
-    const animatePokeball = () => {
-        anime.timeline({
-            targets: pokeballStates,
-            loop: true,
-            update: createRenderPokeballs(),
-        }).add({
-            currentPokeballSize: pokeballSize,
-            delay: function() { return anime.random(0, pokeballAppearDelay); },
-            duration: pokeballAppearTime,
-        }).add({
-            keyframes: [
-                {
-                    rotation: 0,
-                },
-                {
-                    rotation: pokeballRotation,
-                },
-                {
-                    rotation: -pokeballRotation,
-                },
-                {
-                    rotation: 0,
-                },
-            ],
-            easing: 'cubicBezier(0.450, 0.010, 0.010, 1.000)',
-            duration: pokeballWobbleTime,
-            delay: function() { return anime.random(0, pokeballWobbleDelay); },
-        });
-    }
-
-    const runStuff = () => {
-        loaded++;
-        if (loaded !== 2) {
+    useEffect(() => {
+        if (imagesLoaded !== 2 || animating) {
             return;
         }
+        setAnimating(true);
 
-        for (let i = 0; i < numbles; i++) {
+        for (let i = 0; i < NUMBER_OF_POKEMON; i++) {
             pokeballStates.push({
                 currentPokeballSize: 0,
                 rotation: 0,
             })
         }
 
-        canvas = document.getElementById("poke-canvas");
-        ctx = canvas.getContext('2d');
+        ctxRef.current = canvasRef.current.getContext('2d');
 
-        pokeballImg = document.getElementById("pokeball-img");
-        pokeballGreyscaleImg = document.getElementById("pokeball-greyscale-img");
+        anime.timeline({
+            targets: pokeballStates,
+            loop: true,
+            update: renderPokeballs,
+            duration: POKEBALL_APPEAR_TIME + POKEBALL_APPEAR_DELAY + POKEBALL_WOBBLE_TIME + POKEBALL_WOBBLE_DELAY,
+        }).add({
+            currentPokeballSize: pokeballSize,
+            delay: function() { return anime.random(0, POKEBALL_APPEAR_DELAY); },
+            duration: POKEBALL_APPEAR_TIME,
+        }).add({
+            keyframes: [
+                {
+                    rotation: 0,
+                },
+                {
+                    rotation: POKEBALL_ROTATION,
+                },
+                {
+                    rotation: -POKEBALL_ROTATION,
+                },
+                {
+                    rotation: 0,
+                },
+            ],
+            easing: 'cubicBezier(0.450, 0.010, 0.010, 1.000)',
+            duration: POKEBALL_WOBBLE_TIME,
+            delay: function() { return anime.random(0, POKEBALL_WOBBLE_DELAY); },
+        });
+    }, [imagesLoaded, animating, renderPokeballs, pokeballSize])
 
+    const selectPokemon = (event) => {
+        const row = Math.floor(event.pageY / pokeballSize) + 1;
+        const column = Math.floor(event.pageX / pokeballSize) + 1;
+        const pokemonNumber = (row - 1) * columns + column;
+        if (pokemonNumber > NUMBER_OF_POKEMON) {
+            return;
+        }
+        setSelectedPokemon(pokemonNumber);
+        console.log(pokemonList[pokemonNumber - 1])
+    }
 
-        const render = anime({
-            duration: pokeballAppearTime + pokeballAppearDelay + pokeballWobbleTime + pokeballWobbleDelay,
-        })
-
-        setCanvasSize();
-        render.play();
-        animatePokeball();
+    let pokemonToDisplay;
+    if (pokemonList[selectedPokemon - 1] !== undefined) {
+        pokemonToDisplay = <img src={pokemonList[selectedPokemon-1].imageName} alt={pokemonList[selectedPokemon-1].name} />
     }
 
     return (
         <div id="poke-wrapper">
-            <div id="poke-canvas-wrapper">
-                <canvas id="poke-canvas"/>
+            <div ref={canvasWrapperRef} id="poke-canvas-wrapper">
+                <canvas ref={canvasRef} onClick={selectPokemon} id="poke-canvas"/>
             </div>
             <div id="poke-controls">
                 <h1>Controls</h1>
                 <form>
                     <label htmlFor="generation">Generation:</label><br/>
                     <input type="number" id="generation" name="generation" defaultValue="1" min="1" max="8" onChange={(event) => {
-                        highlightThreshold = numInGenerationsTotal[event.target.value - 1];
-                        createRenderPokeballs(true)();
+                        highlightThreshold = NUMBER_OF_POKEMON_PER_GENERATION_TOTAL[event.target.value - 1];
+                        forceRender = true;
                     }} />
                 </form>
+                {pokemonToDisplay}
             </div>
-            <img style={{display: 'none'}} onLoad={runStuff} id="pokeball-img" src="pokeball.svg" alt="" />
-            <img style={{display: 'none'}} onLoad={runStuff} id="pokeball-greyscale-img" src="pokeball-greyscale.svg" alt="" />
+            <img ref={pokeballImgRef} style={{display: 'none'}} onLoad={imageLoaded} id="pokeball-img" src="pokeball.svg" alt="" />
+            <img ref={pokeballGreyscaleImgRef} style={{display: 'none'}} onLoad={imageLoaded} id="pokeball-greyscale-img" src="pokeball-greyscale.svg" alt="" />
         </div>
     );
 }
